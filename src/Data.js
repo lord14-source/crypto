d75d10bb9c20580e70857a1ddf3a131fcd88b15f
lord14-source <85446308+lord14-source@users.noreply.github.com>
2022-05-29 10:56:07 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,52 @@
+export const UserData =[
+    {
+        id:1,
+        year:2016,
+        userGain:80000,
+        userLost:823,
+    }
+    ,
+    {
+        id:2,
+        year:2017,
+        userGain:63527,
+        userLost:803,
+    },
+
+    {
+        id:3,
+        year:2019,
+
+        userGain:80000,
+        userLost:823,
+    },
+    {
+        id:4,
+        year:2019,
+        userGain:65000,
+        userLost:323,
+    }
+    ,
+    {
+        id:5,
+        year:2020,
+        userGain:90000,
+        userLost:123,
+    }
+    ,
+    {
+        id:6,
+        year:2021,
+        userGain:60000,
+        userLost:823,
+    }
+    ,{
+        id:7,
+        year:2022,
+        userGain:60000,
+        userLost:823,
+    }
+    ,
+    
+    
+];
